cdf5badc169fcb5c5a68c592c61769c0c671a825
jonkee <zzzgoo@gmail.com>
2019-08-15 18:35:40 +0800

    3333

--- src/plugin/csName.js
+++ src/plugin/csName.js
@@ -2,7 +2,7 @@ const plugin = {
 	name: "csName",
 	type: "replace",
 	compile(segment) {
-		return `${this.segment}{{cs_name}}`
+		return `${segment}{{cs_name}}`
 	},
 	render(str, config) {
 		let result = str
