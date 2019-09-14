2939d2c3a91ac97416485ccdc5b7a3b31c488085
jonkee <zzzgoo@gmail.com>
2019-09-14 14:35:37 +0800

    bugs

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import ownerName from "./plugin/ownerName"
 import csName from "./plugin/csName"
 import line from "./plugin/line"
 import sanitize from "./plugin/sanitize"
+import emoji from "./plugin/emoji"
 
 markdown.use(color).use(bold)
 	.use([userName, ownerName, csName])
 	.use(line)
 	.use(sanitize)
+	.use(emoji)
 
 export default markdown
